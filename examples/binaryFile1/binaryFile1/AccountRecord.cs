d0d60d7290c8926372a75763a37965068f7bfb3d
jleefish <jleefish@gmail.com>
2016-03-18 11:00:10 -0400

    Revert "Revert "Added example project""
    This reverts commit 73bc57fe6936c5e3bb994ffef6ce09d8401ab83f.

---
+++ examples/binaryFile1/binaryFile1/AccountRecord.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryFile1
+{
+    //class stores data for 1 record
+    //represents SCHEMA
+    class AccountRecord
+    {
+        private const int _RECORD_SIZE = 44;
+
+        //first identify fields
+        private int _accountNumber;
+        private string _firstName;
+        private string _lastName;
+        private double _accountBalance;
+        //second set constructors
+        public AccountRecord()
+        {
+            AccountNumber = 0;
+            FirstName = "";
+            LastName = "";
+            AccountBalance = 0.0;
+        }
+        public AccountRecord(int accountNumber, string firstName, string lastName, double accountBalance)
+        {
+            AccountNumber = accountNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            AccountBalance = accountBalance;
+        }
+        //third set properties (setters/getters)
+        public int AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+            set
+            {
+                _accountNumber = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public double AccountBalance
+        {
+            get
+            {
+                return _accountBalance;
+            }
+            set
+            {
+                _accountBalance = value;
+            }
+        }
+        public int FileIndex
+        {
+            get
+            {
+                return (AccountNumber - 1) * _RECORD_SIZE;
+            }
+        }
+        public static int RecordSize
+        {
+            get
+            {
+                return _RECORD_SIZE;
+            }
+        }
+        public static int getFileIndex(int recordNum)
+        {
+            return (recordNum - 1) * RecordSize;
+        }
+    }
+}
---
+++ examples/binaryFile1/binaryFile1/AccountRecordRA.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryFile1
+{
+    class AccountRecordRA : AccountRecord
+    {
+        private const int _MAX_NAME_LENGTH = 15;
+
+        public AccountRecordRA()
+            : base()
+        {
+            //no arg base constructor called
+        }
+        public AccountRecordRA(int accountNumber, string firstName, string lastName, double accountBalance)
+            : base(accountNumber, firstName, lastName, accountBalance)
+        {
+            //4 arg base constructor called
+        }
+        public void write(FileStream file)
+        {
+            BinaryWriter bw = new BinaryWriter(file);
+            //fields must be written in order
+            bw.Write(AccountNumber);
+            bw.Write(toFixedString(FirstName));
+            bw.Write(toFixedString(LastName));
+            bw.Write(AccountBalance);
+        }
+        private string toFixedString(string n)
+        {
+            StringBuilder sb = new StringBuilder(n);
+            sb.Length = _MAX_NAME_LENGTH;
+            return sb.ToString();
+        }
+        public void read(FileStream file)
+        {
+            BinaryReader br = new BinaryReader(file);
+            AccountNumber = br.ReadInt32();
+            //null characters need to be removed
+            FirstName = br.ReadString().Replace('\0', ' ');
+            LastName = br.ReadString().Replace('\0', ' ');
+            AccountBalance = br.ReadDouble();
+        }
+        public void delete(FileStream file){
+            AccountNumber = 0;
+            FirstName = "";
+            LastName = "";
+            AccountBalance = 0.0;
+            write(file);
+        }
+    }
+}
