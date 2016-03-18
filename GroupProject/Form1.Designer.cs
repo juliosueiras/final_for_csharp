f5cc3861668d9a99f0e3e317278223a713e636fc
Dan Newton <Dan Newton>
2016-03-18 11:17:22 -0400

    finished rought implementation of form load without record or tablefactory

--- GroupProject/Form1.Designer.cs
+++ GroupProject/Form1.Designer.cs
@@ -28,16 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.cmd_Insert = new System.Windows.Forms.Button();
             this.cmd_Update = new System.Windows.Forms.Button();
             this.cmd_Delete = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_ExpLevel = new System.Windows.Forms.TextBox();
+            this.txt_YearsExp = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_desciption = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // cmd_Insert
             // 
-            this.cmd_Insert.Location = new System.Drawing.Point(234, 168);
+            this.cmd_Insert.Location = new System.Drawing.Point(15, 91);
             this.cmd_Insert.Name = "cmd_Insert";
             this.cmd_Insert.Size = new System.Drawing.Size(75, 23);
             this.cmd_Insert.TabIndex = 0;
@@ -46,7 +59,7 @@
             // 
             // cmd_Update
             // 
-            this.cmd_Update.Location = new System.Drawing.Point(315, 168);
+            this.cmd_Update.Location = new System.Drawing.Point(96, 91);
             this.cmd_Update.Name = "cmd_Update";
             this.cmd_Update.Size = new System.Drawing.Size(75, 23);
             this.cmd_Update.TabIndex = 1;
@@ -55,7 +68,7 @@
             // 
             // cmd_Delete
             // 
-            this.cmd_Delete.Location = new System.Drawing.Point(396, 168);
+            this.cmd_Delete.Location = new System.Drawing.Point(177, 91);
             this.cmd_Delete.Name = "cmd_Delete";
             this.cmd_Delete.Size = new System.Drawing.Size(75, 23);
             this.cmd_Delete.TabIndex = 2;
@@ -64,17 +77,124 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(234, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 120);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(425, 237);
             this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Skill ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Skill Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Experience Level";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Years  of Experience";
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Location = new System.Drawing.Point(15, 25);
+            this.txt_ID.MaxLength = 3;
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.ShortcutsEnabled = false;
+            this.txt_ID.Size = new System.Drawing.Size(100, 20);
+            this.txt_ID.TabIndex = 8;
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(124, 25);
+            this.txt_Name.MaxLength = 32;
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.ShortcutsEnabled = false;
+            this.txt_Name.Size = new System.Drawing.Size(100, 20);
+            this.txt_Name.TabIndex = 9;
+            // 
+            // txt_ExpLevel
+            // 
+            this.txt_ExpLevel.Location = new System.Drawing.Point(231, 25);
+            this.txt_ExpLevel.MaxLength = 32;
+            this.txt_ExpLevel.Name = "txt_ExpLevel";
+            this.txt_ExpLevel.ShortcutsEnabled = false;
+            this.txt_ExpLevel.Size = new System.Drawing.Size(100, 20);
+            this.txt_ExpLevel.TabIndex = 10;
+            // 
+            // txt_YearsExp
+            // 
+            this.txt_YearsExp.Location = new System.Drawing.Point(337, 25);
+            this.txt_YearsExp.MaxLength = 2;
+            this.txt_YearsExp.Name = "txt_YearsExp";
+            this.txt_YearsExp.ShortcutsEnabled = false;
+            this.txt_YearsExp.Size = new System.Drawing.Size(100, 20);
+            this.txt_YearsExp.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Description";
+            // 
+            // txt_desciption
+            // 
+            this.txt_desciption.Location = new System.Drawing.Point(15, 65);
+            this.txt_desciption.MaxLength = 128;
+            this.txt_desciption.Name = "txt_desciption";
+            this.txt_desciption.ShortcutsEnabled = false;
+            this.txt_desciption.Size = new System.Drawing.Size(422, 20);
+            this.txt_desciption.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(486, 261);
+            this.ClientSize = new System.Drawing.Size(457, 374);
+            this.Controls.Add(this.txt_desciption);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_YearsExp);
+            this.Controls.Add(this.txt_ExpLevel);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.txt_ID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.cmd_Delete);
             this.Controls.Add(this.cmd_Update);
@@ -83,7 +203,9 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,6 +215,17 @@
         private System.Windows.Forms.Button cmd_Update;
         private System.Windows.Forms.Button cmd_Delete;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_ExpLevel;
+        private System.Windows.Forms.TextBox txt_YearsExp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_desciption;
+        private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
 
--- GroupProject/Form1.cs
+++ GroupProject/Form1.cs
@@ -18,22 +18,114 @@ namespace GroupProject
             InitializeComponent();
         }
 
+        int maxRecords = 100;
+        int fileSize = 0; // TODO: Adjust file length based on record
+        string fileName = "skills.dat";
         FileStream file;
         DataTable table;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Init table
-            //Init file
-            // TODO: Implement Me
+            Initialize();
             dataGridView1.Click += dataGridView1_Click;
             cmd_Delete.Click += cmd_Delete_Click;
             cmd_Insert.Click += cmd_Insert_Click;
             cmd_Update.Click += cmd_Update_Click;
+            txt_ID.KeyPress += txt_ID_KeyPress;
+            txt_Name.KeyPress += txt_Name_KeyPress;
+            txt_ExpLevel.KeyPress += txt_ExpLevel_KeyPress;
+            txt_YearsExp.KeyPress += txt_YearsExp_KeyPress;
+            txt_desciption.KeyPress += txt_desciption_KeyPress;
+        }
+
+        private void Initialize()
+        {
+            try
+            {
+                file = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                if (file.Length != fileSize)
+                {
+                    initData();
+                }
+                CreateTableAndDisplay();
+                ReadFile();
+            }
+            catch (IOException io)
+            {
+                DisplayErrorMessage(io.Message, "Failed to Create File");
+            }
+            //Disable right click
+            txt_ID.ContextMenuStrip = new ContextMenuStrip();
+            txt_Name.ContextMenuStrip = new ContextMenuStrip();
+            txt_ExpLevel.ContextMenuStrip = new ContextMenuStrip();
+            txt_YearsExp.ContextMenuStrip = new ContextMenuStrip();
+            txt_desciption.ContextMenuStrip = new ContextMenuStrip();
+        }
+
+        private void initData()
+        {
+            Object record = null; // TODO: integrate record class
+            try
+            {
+                //position file pointer:
+                file.Seek(0, SeekOrigin.Begin);
+                for (int i = 0; i < maxRecords; i++)
+                {
+                    // TODO: integrate record class
+                    //record.write(file);
+                }
+                ReadFile();
+            }
+            catch (IOException io)
+            {
+                DisplayErrorMessage(io.Message, "Failed to Initialize File");
+            }
+        }
+
+        void CreateTableAndDisplay()
+        {
+            // TODO: Integrate table factory
+            //table = TableFactory.makeTable();
+            bindingSource1.DataSource = table;
+            dataGridView1.DataSource = bindingSource1;
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+        void txt_desciption_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // TODO: Implement Me
+            throw new NotImplementedException();
+        }
+
+        void txt_YearsExp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // TODO: Implement Me
+            throw new NotImplementedException();
+        }
+
+        void txt_ExpLevel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // TODO: Implement Me
+            throw new NotImplementedException();
+        }
+
+        void txt_Name_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // TODO: Implement Me
+            //throw new NotImplementedException();
+        }
+
+        void txt_ID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // TODO: Implement Me
+            throw new NotImplementedException();
         }
 
         void cmd_Update_Click(object sender, EventArgs e)
         {
+            // TODO: Implement Me
             throw new NotImplementedException();
         }
 
@@ -80,6 +172,11 @@ namespace GroupProject
         void SetControlState(string state)
         {
             // TODO: Implement
+        }
+
+        void DisplayErrorMessage(string message, string title)
+        {
+            MessageBox.Show(message, "ERROR: " + title, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
